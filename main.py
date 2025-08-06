ca9ded33ca7c6209edd1f848f70cb19a04015395
santiago olarte <zweiolarte@gmail.com>
2025-08-05 20:12:07 -0500

    cambiamos la suma por la multiplicacion

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ def main():
     print("Pilon")
     numero1= float(input("Ingrese el primer número: "))
     numero2 = float(input("Ingrese el segundo número: "))
-    suma = numero1 + numero2
-    if suma > 10:
-        print("La suma es mayor que 10")
+    multiplicacion = numero1 * numero2
+    if multiplicacion > 10:
+        print("La multiplicación es mayor que 10")
     else:
-        print("La suma es menor o igual a 10")
-    print("La suma es:", suma)
+        print("La multiplicación es menor o igual a 10")
+    print("La multiplicación es:", multiplicacion)
 
 if __name__ == "__main__":
     main()
