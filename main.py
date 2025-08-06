6de91c66996c4dce972b7da74d850bbb92d40e99
santiago olarte <zweiolarte@gmail.com>
2025-08-05 20:52:29 -0500

    2darama ensayo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def main():
     print("Pilon")
+    print("2darama ensayo")
     numero1= float(input("Ingrese el primer número: "))
     numero2 = float(input("Ingrese el segundo número: "))
     multiplicacion = numero1 * numero2
