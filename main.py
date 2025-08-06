17187629be6f54b403079a60690100bd194e684a
santiago olarte <zweiolarte@gmail.com>
2025-08-05 20:47:57 -0500

    fin del programa master

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ def main():
     print("Pilon")
     numero1= float(input("Ingrese el primer número: "))
     numero2 = float(input("Ingrese el segundo número: "))
-    multiplicacion = numero1 * numero2
+    Multiplicacion = numero1 * numero2
     if multiplicacion > 10:
-        print("La multiplicación es mayor que 10")
+        print("La multiplicacion es mayor que 10")
     else:
-        print("La multiplicación es menor o igual a 10")
-    print("La multiplicación es:", multiplicacion)
+        print("La multiplicacion es menor o igual a 10")
+    print("La multiplicacion es:", multiplicacion)
+
+    print("Fin del programa master")
 
 if __name__ == "__main__":
     main()
