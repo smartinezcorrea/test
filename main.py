4e02481c5dc63d0d0e93c7b7cec3cb987e084ceb
santiagom <smartinezcorrea88@outlook.com>
2025-08-13 17:59:40 -0500

    init

---
+++ main.py
@@ -0,0 +1,18 @@
+def main():
+    print ("hola esta es la 2da rama")
+    numero1= float(input("ingrese el primer numero"))
+    numero2= float(input("ingrese el segundo numero"))
+    olarte= numero1*numero2
+    if olarte > 10:
+        print("la multiplicacion es mayor a 10")
+    else:
+        print("la multiplicacion es menor o igual a 10")
+    print("la multiplicacion es:", olarte)
+    print("mensaje del master")
+
+
+
+
+
+if __name__ == "__main__":
+    main()
